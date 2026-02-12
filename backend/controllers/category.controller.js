8908ba0ecf34c2221c6afcb2c48ea70c7dc0b97d
Pheareak <roeurnpheareak71@gmail.com>
2026-02-12 18:49:55 +0700

    upload backend

---
+++ backend/controllers/category.controller.js
@@ -0,0 +1,21 @@
+const addCategory = (req, res) => {
+    try{
+
+        res.status(200).json({
+            success: true,
+            message: "running..."
+        })
+
+    }catch(error){
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+
+    }
+}
+
+module.exports = {
+    addCategory,
+}
